7dfd3938c1d4c14245ec65b1f10f1f83626a1699
github username <github email address>
2019-04-12 21:47:13 -0700

    added persist drawing

--- src/components/Canvas.js
+++ src/components/Canvas.js
@@ -2,7 +2,7 @@ import React, { Component, Fragment } from 'react';
 import { ActionCable } from 'react-actioncable-provider';
 import { API_ROOT } from '../constants';
 // import Cable from './Cable';
-
+const API = "http://localhost:4000/draw";
 
 class Canvas extends React.Component {
   constructor (props) {
@@ -20,7 +20,17 @@ class Canvas extends React.Component {
       x: 'black',
       y: 2,
       width: 400, // make this a prop
-      height: 400 // make this a prop
+      height: 400, // make this a prop
+      prevxArray: [],
+      prevyArray: [],
+      currentxArray: [],
+      currentyArray:[],
+      movement: {
+        previousX: [],
+        previousY: [],
+        currentX: [],
+        currentY: []
+      }
     }
   }
 
@@ -44,12 +54,13 @@ class Canvas extends React.Component {
 
   handleMouseMoves = (event, action) => {
     event.persist()
-    console.log('moving the mouse', action, event)
+    // console.log('moving the mouse', action, event)
     this.findxy(action, event)
   }
 
   findxy = (mouseAction, e) => {
-    if (mouseAction == 'down') {
+    if (mouseAction === 'down') {
+
       this.setState(
         (state) => {return {
           prevX: state.currX,
@@ -70,9 +81,28 @@ class Canvas extends React.Component {
           }
         }
       )
-    } else if (mouseAction == 'up' || mouseAction == "out") {
-        this.setState({flag: false})
-    } else if (mouseAction == 'move' && this.state.flag) {
+    } else if (mouseAction === 'up' || mouseAction === "out") {
+        let data = {flag: false,
+          movement: {...this.state.movement,
+          previousX: this.state.prevxArray,
+          previousY: this.state.prevyArray,
+          currentX: this.state.currentxArray,
+          currentY: this.state.currentyArray
+        }}
+        this.setState(data)
+
+        fetch(API,{
+          method: "POST",
+          headers: {'Content-Type':'application/json'},
+          body: JSON.stringify({
+            data
+          })
+        })
+
+
+
+        console.log(this.state.movement)
+    } else if (mouseAction === 'move' && this.state.flag) {
       this.setState(
         (state) => {
           return {
@@ -93,18 +123,48 @@ class Canvas extends React.Component {
     this.state.ctx.beginPath();
     this.state.ctx.moveTo(this.state.prevX, this.state.prevY);
     this.state.ctx.lineTo(this.state.currX, this.state.currY);
+    //get movement datas into an object of arrays
+    this.state.prevxArray.push(this.state.prevX);
+    this.state.prevyArray.push(this.state.prevY);
+    this.state.currentxArray.push(this.state.currX);
+    this.state.currentyArray.push(this.state.currY);
+    //end
     this.state.ctx.strokeStyle = this.state.x;
     this.state.ctx.lineWidth = this.state.y;
     this.state.ctx.stroke();
     this.state.ctx.closePath();
+    //console.log("check calls")
+
+  }
+
+  handleRedraw = () =>{
+    fetch(API)
+    .then(resp => resp.json())
+    .then(json =>{
+      console.log('check this json', json.data.movement)
+
+      for(let i=0; i <json.data.movement.previousX.length; i++){
+        console.log("i am",i)
+         this.state.ctx.beginPath();
+
+         this.state.ctx.moveTo(json.data.movement.previousX[i], json.data.movement.previousY[i]);
+
+         this.state.ctx.lineTo(json.data.movement.currentX[i], json.data.movement.currentY[i]);
+
+         this.state.ctx.strokeStyle = this.state.x;
+         this.state.ctx.lineWidth = this.state.y;
+         this.state.ctx.stroke();
+         this.state.ctx.closePath();
+      }
+    })
   }
 
   render() {
     return (
       <Fragment>
+        <input type="button" value="show drawing" onClick={this.handleRedraw}/>
         {
           this.props.isDrawing ?
-
             <canvas
             ref={this.canvasRef}
             onMouseMove={(event) => this.handleMouseMoves(event, 'move')}
