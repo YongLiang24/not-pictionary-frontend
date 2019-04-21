33ed5a6dc2dd7ca5dddd402bffdef06018c76730
github username <github email address>
2019-04-20 23:44:26 -0700

    changed router>

--- src/index.js
+++ src/index.js
@@ -2,13 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { Router } from 'react-router';
+import createHashHistory from 'history/createHashHistory';
 
 // import registerServiceWorker from './registerServiceWorker';
 import { API_WS_ROOT } from './constants';
 import * as serviceWorker from './serviceWorker';
 
+const hashHistory = createHashHistory({ basename: process.env.PUBLIC_URL });
 ReactDOM.render(
-    <App />,
+  <Router history={hashHistory}>
+    <App />
+  </Router>,
   document.getElementById('root')
 );
 
