dc4759c59dfd764e2b784aab93ee69852aa96ede
github username <github email address>
2019-04-20 22:03:12 -0700

    Updates

---
+++ precache-manifest.8a834006f58c7e8ef2b322c01e6f0057.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "bf6f2b1718ce5694c9d7",
+    "url": "/my-app/static/css/main.6ab89311.chunk.css"
+  },
+  {
+    "revision": "bf6f2b1718ce5694c9d7",
+    "url": "/my-app/static/js/main.0b0e21c2.chunk.js"
+  },
+  {
+    "revision": "9930044c31793394defe",
+    "url": "/my-app/static/js/runtime~main.6019dd84.js"
+  },
+  {
+    "revision": "968208471284e7ddb13a",
+    "url": "/my-app/static/js/2.1432a2f9.chunk.js"
+  },
+  {
+    "revision": "0781af8e50b0710347eedc78b8bc8aaf",
+    "url": "/my-app/static/media/yellow_circle.0781af8e.png"
+  },
+  {
+    "revision": "fb030e38e07229943d561fb00697c7e1",
+    "url": "/my-app/index.html"
+  }
+];
