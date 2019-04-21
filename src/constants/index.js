72b98457df2e25e7f39b536284878d43aa3191e1
github username <github email address>
2019-04-20 22:10:01 -0700

    added static json

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,6 +1,6 @@
 // export const API_ROOT = 'https://pictionaries.herokuapp.com/'
 export const API_ROOT = 'http://pictionaries.herokuapp.com/'
-export const API_WS_ROOT = 'ws://pictionaries.herokuapp.com/cable'
+//export const API_WS_ROOT = 'ws://pictionaries.herokuapp.com/cable'
 export const HEADERS = {
   'Content-Type': 'application/json',
   Accept: 'application/json'
--- src/index.js
+++ src/index.js
@@ -2,15 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import { ActionCableProvider } from 'react-actioncable-provider';
+
 // import registerServiceWorker from './registerServiceWorker';
 import { API_WS_ROOT } from './constants';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
-  <ActionCableProvider url={API_WS_ROOT}>
-    <App />
-  </ActionCableProvider>,
+    <App />,
   document.getElementById('root')
 );
 
